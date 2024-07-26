f5771a6c70b25e2d9e499e57eef40a5c2bfc1460
ASIM\asims <asimshaikh161@gmail.com>
2024-07-25 20:38:26 -0400

    drawRandomArt function added

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"math/rand"
+
+	"github.com/fogleman/gg"
 )
 
+func drawRandomArt(dc *gg.Context, seed int64) {
+	rand.Seed(seed)
+	for i := 0; i < 10; i++ {
+		x := rand.Float64() * float64(dc.Width())
+		y := rand.Float64() * float64(dc.Height())
+		r := rand.Float64() * 0.5 * float64(dc.Width())
+		dc.DrawCircle(x, y, r)
+		dc.SetRGB(rand.Float64(), rand.Float64(), rand.Float64())
+		dc.Fill()
+
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", nil)
+
 }
