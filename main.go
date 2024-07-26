146f80a4560d771fd83a7609a0641486caa10f67
Kristina <kristinashrestha2055@gmail.com>
2024-07-25 20:35:03 -0400

    Adding function handler

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	dc := gg.NewContext(800, 800)
+	hostname, _ := os.Hostname()
+	seed := time.Now().UnixNano() + int64(hostname[0])
+	drawRandomArt(dc, seed)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Starting server on :8080")
