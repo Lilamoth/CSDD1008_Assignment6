0f2b30f6ff26dbb0dad967407da30e67cb234f86
Lilamoth <ahmadraja@loyalistcollege.com>
2024-07-25 20:07:32 -0400

    Initialized main function

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
-//go code
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", nil)
+}
